c99c273ea33f643e1ea0bb3104bda97fb3432e53
Michael Sanders <mike.gamedev@gmail.com>
2019-12-05 16:42:31 -0800

    Initial commit.

---
+++ Day-2a/Intcode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day_2a
+{
+    class Intcode
+    {
+        private int _pointer;
+        private readonly int[] _code;
+
+        public Intcode(int[] code) => _code = code;
+
+        public void Process()
+        {
+            
+            while (true)
+            {
+                int command = ConsumeOpCode();
+                int a, b, t;
+                switch (command)
+                {
+                    case 1:
+                        a = ConsumeOpCode();
+                        b = ConsumeOpCode();
+                        t = ConsumeOpCode();
+                        _code[t] = _code[a] + _code[b];
+                        break;
+                    case 2:
+                        a = ConsumeOpCode();
+                        b = ConsumeOpCode();
+                        t = ConsumeOpCode();
+                        _code[t] = _code[a] * _code[b];
+                        break;
+                    case 99:
+                        return;
+                }
+            }
+        }
+
+        public void PrintValue(int index) => Console.WriteLine(_code[index]);
+
+        private int ConsumeOpCode() => _pointer < _code.Length ? _code[_pointer++] : 99;
+    }
+}
---
+++ Day-2a/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day_2a
+{
+    class Program
+    {
+        private const string InputPath = @"day2a-input.txt";
+
+        static void Main(string[] args)
+        {
+            int[] code = File.ReadAllText(InputPath).Split(",").Select(int.Parse).ToArray();
+
+            code[1] = 12;
+            code[2] = 2;
+
+            Intcode processor = new Intcode(code);
+            processor.Process();
+            processor.PrintValue(0);
+
+            Console.ReadKey();
+        }
+    }
+}
